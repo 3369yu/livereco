f98d9ed042e851f4409e724fd76fa0d8f516acd8
3369yu <151356820+3369yu@users.noreply.github.com>
2024-02-25 13:16:40 +0000

    event show途中

--- app/controllers/public/events_controller.rb
+++ app/controllers/public/events_controller.rb
@@ -6,18 +6,34 @@ class Public::EventsController < ApplicationController
   
   def create
     @event = Event.new
+    if @event.save
+       flash[:notice] = "スケジュールを登録しました"
+       redirect_to events_path
+    else
+       render :index
+    end
   end
   
   def index
+    @event = Event.all
   end
   
   def show
+    @enent = Event.find(params[:id])
   end
   
   def edit
   end
   
   def update
+  end
+  
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/noimage.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
   end
   
   private
