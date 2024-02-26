c689948a26a8d1aa868ca4a3cb59a889e7108c36
3369yu <151356820+3369yu@users.noreply.github.com>
2024-02-26 11:11:32 +0000

    [create]event show

--- app/controllers/public/events_controller.rb
+++ app/controllers/public/events_controller.rb
@@ -1,44 +1,37 @@
 class Public::EventsController < ApplicationController
-  
+
   def new
     @event = Event.new
   end
-  
+
   def create
-    @event = Event.new
+    @event = Event.new(event_params)
+    @event.user_id = current_user.id
     if @event.save
        flash[:notice] = "スケジュールを登録しました"
-       redirect_to events_path
+       redirect_to event_path(@event.id)
     else
        render :index
     end
   end
-  
+
   def index
     @event = Event.all
   end
-  
+
   def show
-    @enent = Event.find(params[:id])
+    @event = Event.find(params[:id])
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
-  def get_image
-    unless image.attached?
-      file_path = Rails.root.join('app/assets/images/noimage.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    end
-    image.variant(resize_to_limit: [width, height]).processed
-  end
-  
+
   private
   def event_params
     params.require(:event).permit(:name, :event_data, :place, :open, :start, :title, :price, :buy, :imag, :seet, :transportation, :stay, :impression, :setlist, :is_active)
   end
-  
+
 end
