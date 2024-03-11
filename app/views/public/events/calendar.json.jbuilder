e5ea750f3dce028f5587c31ae314503feaff29b1
3369yu <151356820+3369yu@users.noreply.github.com>
2024-03-11 06:16:06 +0000

    [update]FullCalendar イベント表示

--- app/views/public/events/calendar.json.jbuilder
+++ app/views/public/events/calendar.json.jbuilder
@@ -1,6 +1,18 @@
 json.array!(@events) do |event|
   json.id event.id
+  json.user_id event.user_id
   json.name event.name
+  json.data event.event_data.in_time_zone('Tokyo')
+  json.place event.place
+  json.open event.open
   json.start event.start.in_time_zone('Tokyo')
-  json.end event.end.in_time_zone('Tokyo')
+  json.title event.title
+  json.price event.price
+  json.buy event.buy
+  json.seet event.seet
+  json.transportation event.transportation
+  json.stay event.stay
+  json.impression event.impression
+  json.setlist event.setlist
+  json.status event.status
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,31 +2,32 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations], controllers: {
     sessions: "admin/sessions"
   }
-  
+
   devise_for :users, controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
+
   scope module: :public do
-    root to: "homes#top"
+    root to: "events#index"
+    get '/events', to: 'events#index', defaults: { format: 'json' }
+    get '/home' => 'homes#top'
     get 'homes/about' => 'homes#about', as: :about
-    resources :users, only: [:show, :edit, :update, :destroy] 
-    
-    resources :events do
-      get '/events', to: 'events#index', defaults: { format: 'json' }
+    resources :users, only: [:show, :edit, :update, :destroy]
+
+    resources :events, only: [:show, :new, :edit, :create, :update, :destroy] do
       collection do
         get 'history'
       end
       resource :favorite, only: [:create, :destroy]
       end
     end
-    
+
   namespace :admin do
     root to: "users#index"
     resources :users, only: [:index, :show, :edit, :update, :destroy]
   end
-  
+
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
