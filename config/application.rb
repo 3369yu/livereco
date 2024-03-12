92129272b6b9874c42412965e819523b96b780ad
3369yu <151356820+3369yu@users.noreply.github.com>
2024-03-12 12:09:02 +0000

    [update]TimeZone

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Livereco
 
     config.i18n.default_locale = :ja
 
-    #config.time_zone = "Tokyo"
+    config.time_zone = 'Asia/Tokyo'
     #config.active_record.default_timezone = :local
 
     # Configuration for the application, engines, and railties goes here.
