44c7ff2a84a43126dc0bed3aeaaa480e659542d1
3369yu <151356820+3369yu@users.noreply.github.com>
2024-03-15 03:27:50 +0000

    [update]admin users レイアウト調整

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,8 +1,8 @@
 class Admin::UsersController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def index
-    @users = User.all
+    @users = User.page(params[:page])
   end
 
   def show
@@ -12,12 +12,26 @@ class Admin::UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_users_path
+    else
+      render :edit
+    end
+  end
+
   def destory
     @user = User.find(params[:id])
     @user.destroy
     flash[:notice] = "退会しました。"
     redirect_to :root
   end
-  
+
+  private
+   def user_params
+    params.require(:user).permit(:name, :profile_image)
+   end
+
 end
