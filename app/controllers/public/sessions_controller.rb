ed7912e7ac41b1c8f69551a3408d3ac3ce1683ab
3369yu <151356820+3369yu@users.noreply.github.com>
2024-03-16 03:06:49 +0000

    [update]guestuser_login

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -7,6 +7,10 @@ class Public::SessionsController < Devise::SessionsController
     events_path
   end
   
+  def after_sign_in_path_for(resource)
+    events_path
+  end
+  
   def after_sign_out_path_for(resource)
     root_path
   end
