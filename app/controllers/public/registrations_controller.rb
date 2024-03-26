647d30056149724097cf2dc3dbb169b037ef12f1
3369yu <151356820+3369yu@users.noreply.github.com>
2024-03-26 12:54:53 +0000

    [update]password変更

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -2,13 +2,13 @@
 
 class Public::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  #before_action :configure_account_update_params, only: [:update]
 
   def after_sign_up_path_for(resource)
     user_path(current_user.id)
   end
 
-  def configure_account_update
+  def after_update_path_for(resource)
     user_path(current_user.id)
   end
 
